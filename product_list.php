7a0f61f481794082243362a2c82903d48ec8783b
aomom000 <6106021411097@fitm.kmutnb.ac.th>
2021-04-01 15:38:23 +0700

    update

--- product_list.php
+++ product_list.php
@@ -59,21 +59,45 @@
         }
         text += "<tr><td>"+label[6]+"</td>";
         text += "<td><input type='number' id='n"+idx+"' min='1' max='"+arr[idx][6]+"'></td></tr>";
-        text += "<tr><td colspan='2'><button onclick='open_po("+idx+","+cus_id+")'>add to cart</button><input type='reset'></td></tr>"
+        text += "<tr><td colspan='2'><button onclick='open_bill("+idx+","+cus_id+")'>add to cart</button><input type='reset'></td></tr>"
         text += "</table>";
         out.innerHTML = text;
     }
 
-    function open_po(idx,cus_id){
+    function open_bill(idx,cus_id){
         qty = document.getElementById("n"+idx);
+        out3 = document.getElementById("out3");
         price = arr[idx][5];
         //alert("product_code="+arr[idx][1]+"="+qty.value+",price="+price);
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){ 
             if(this.readyState==4 && this.status==200){
-                alert(this.responseText);
+                //alert(this.responseText);
+                arr2 = JSON.parse(this.responseText);
+                t1 = JSON.parse(arr2["bill"])[0];
+                text = "<table border = '1'><tr>";
+                text += "<tr><th>Bill Id</th><th>CUS_ID</th><th>EmpID</th><th>Bill Date</th><th>Bill Status</th><th>Remas</th></tr>";
+                for(x=0;x<=t1.length;x++){
+                    text += "<td>"+t1[i]+"</td>";
+                }
+                text += "</tr></table>";
+                out3.innerHTML = text;
+                text = "<table border='1'>";
+                for(i=0;i<label.length-1;i++){
+                    text += "<th>"+label[i]+"</th>";
+                }
+                text = "<tr>"+text+"</tr>";
+                for(i=0;i<arr.length;i++){
+                    for(j=0;j<arr[i].length-1;j++){
+                        text += "<td>"+arr[i][j]+"</td>";
+                    }
+                    text += "<td>"+"<button onclick='sel_product("+i+")'>< ShopShock ></button>"+"<td>";
+                    text = "<tr>"+text+"</tr>";
+                }
+                text += "</table>";
             }
         }
+        
         xhttp.open("POST","product_rest.php",true);
         xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xhttp.send("p_id="+arr[idx][0]+"&p_qty="+qty.value+"&p_price="+price+"&cus_id="+cus_id);
--- product_rest.php
+++ product_rest.php
@@ -13,7 +13,12 @@
     function product_list(){
         $db = new database();
         $db->connect();
-        $sql = "SELECT * FROM product"; 
+        $sql = "SELECT Product_id,Product_code,Product_Name,
+                        brand.Brand_name, unit.Unit_name,
+                        product.Cost, product.Stock_Quantity
+        FROM   product,brand,unit
+        WHERE  product.Brand_ID = brand.Brand_id 
+        and    product.Unit_ID = unit.Unit_id"; 
         $result = $db->query($sql);
         $db->close();
         return $result;
@@ -27,6 +32,8 @@
         //          1.2.1a check product id exist yes: update qty in bill_detail
         //          1.2.2b check product id exist on: add product to bill_detail
         //    1.2.2 no:  
+        $step=1; $bill_id=1; $bill_head=""; $bill_detail="";
+
         $db = new database();
         $db->connect();
         $sql = "SELECT Bill_id,Bill_Status FROM bill WHERE Cus_Id='{$_SESSION['cus_id']}' order by Bill_id desc limit 1";
@@ -44,26 +51,27 @@
         }else{
             //check [0][0] billid 
             //      [0][1] bill status
-            if($bill_result[0][1]==0){
-                $sql = "SELECT Bill_id,Product_ID FROM bill_detail 
-                        WHERE Bill_id = '{$_SESSION['cus_id']}' 
-                        and Product_ID = '{$p_id}'";
-                $result = $db->query($sql);
-                if(sizeof($result)==0){
+            $step="3:add new item";
+                if($bill_result[0][1]==0){
                     //add new product
                     $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) 
                             VALUES ({$bill_result[0][0]},{$p_id},{$p_qty},{$p_price})";
                     $result = $db->exec($sql);
-                }else{
-                    //update current item
-                    $sql = "UPDATE 'bill_detail' SET 'Bill_id'={$bill_result[0][0]},'Product_ID'={$p_id},'Quantity'={$p_qty},'Unit_Price'={$p_price} 
-                    WHERE 'Product_ID' = {$p_id}"; 
-                    $result = $db->exec($sql);
+                    if($result ==0){
+                        $step="4:update item";
+                        $bill_id = $bill_result[0][0];
+                        $sql2 = "UPDATE bill_detail SET Quantity={$p_qty},Unit_Price={$p_price} WHERE Bill_id={$bill_id} and Product_ID={$p_id}";
+                        $result = $db->exec($sql2);
+                        $step="5:update complete";
+                    }
+                    $sql = "Select * from bill where Bill_Id={$bill_result[0][0]}";
+                    $bill_head = $db->query($sql);
+                    $sql = "Select * from bill_detail where Bill_Id={$bill_result[0][0]}";
+                    $bill_detail = $db->query($sql);
                 }
             }
-        }
     #INSERT INTO `bill`(`Bill_id`, `Cus_ID`, `Bill_Status`) VALUES (1,1,1)
-    return $result;
+    return ["step"=>$step, "bill"=>json_encode($bill_head), "bill_detail"=>$bill_detail];
     }
     
 ?>
